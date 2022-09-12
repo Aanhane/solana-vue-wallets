9ccf82a95d97a184f269aea746da4bd48545495b
Rob <robaanhane@gmail.com>
2022-09-12 09:47:28 +0200

    fix: clear storage on disconnect click

--- src/components/WalletModalProvider.vue
+++ src/components/WalletModalProvider.vue
@@ -16,7 +16,7 @@ export default defineComponent({
   },
   setup(props, { slots }) {
     const { featured, container, logo, dark } = toRefs(props);
-    const modalPanel = ref<HTMLElement | null>(null);
+    const modalPanel = ref<HTMLElement>();
     const modalOpened = ref(false);
     const openModal = () => modalOpened.value = true;
     const closeModal = () => modalOpened.value = false;
